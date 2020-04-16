ed5ad6b60c613f23626b2a21b309edb57f9736b4
bala2explore <bala2explore@gmail.com>
2020-04-16 12:29:58 +0530

    Initial Class

---
+++ src/BestTimeToBuy.java
@@ -0,0 +1,17 @@
+public class BestTimeToBuy{
+
+
+    public int maxProfit(int[] prices){
+
+        int max = 0;
+
+        int min = Integer.MAX_VALUE;
+
+        System.out.println();
+
+
+
+    }
+
+
+}
